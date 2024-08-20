33be53c9b75842b22d8599c4657f4a6238ea3f02
hlulanimelvin <melvin.shivambu@gmail.com>
2024-08-20 09:50:03 +0200

    Add files via upload

---
+++ BasicFrameApp.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package basicframeapp;
+
+import za.ac.tut.ui.MyFirstFrame;
+
+/**
+ *
+ * @author hlulani.shivambu
+ */
+public class BasicFrameApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //declare variables
+        MyFirstFrame myframe;
+        
+        //instatiate the variable
+        
+        myframe =new MyFirstFrame();
+    }
+    
+}
